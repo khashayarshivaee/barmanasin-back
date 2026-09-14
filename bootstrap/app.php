3734acfbb3f03f7e26390a9e4abefaaf6f8edc4a
khashayarshivaee <154893891+khashayarshivaee@users.noreply.github.com>
2026-09-14 13:44:57 +0330

    Restore Sanctum token authentication for mail 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
         __DIR__.'/../routes/channels.php',
         [
             'middleware' => [
-                'web',
+                'api',
+                'auth:sanctum',
             ],
         ],
     )
