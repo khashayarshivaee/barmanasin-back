ae505247a7513463a83b4976ec435711bfd54f93
khashayarshivaee <154893891+khashayarshivaee@users.noreply.github.com>
2026-09-14 09:44:41 +0330

    Add mail folder stats reader support 2

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,7 @@ use App\Http\Controllers\Api\MailDraftUpdateController;
 use App\Http\Controllers\Api\MailDraftDeleteController;
 use App\Http\Controllers\Api\MailAvatarController;
 use App\Http\Controllers\Api\MailSearchController;
+use App\Http\Controllers\Api\MailFolderStatsController;
 Route::get('/header/menu', [HeaderMenuController::class, 'index'])
     ->name('header.menu');
 
@@ -102,6 +103,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get(
         '/mail/inbox',
         MailInboxController::class
+    );
+
+    Route::get(
+        '/mail/folder-stats',
+        MailFolderStatsController::class
     );
 
     Route::get(
