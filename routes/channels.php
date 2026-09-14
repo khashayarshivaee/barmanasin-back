e90266013ecaf387b23392f8eb73bfd6f3865f3e
khashayarshivaee <154893891+khashayarshivaee@users.noreply.github.com>
2026-09-14 11:57:31 +0330

    Add Sanctum personal access tokens table 6

--- routes/channels.php
+++ routes/channels.php
@@ -6,11 +6,17 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
+
 Broadcast::channel(
     'mailbox.{userId}',
     function ($user, $userId) {
 
-        return (int) $user->id === (int) $userId;
+        \Log::info('Broadcast debug', [
+            'user_id' => $user?->id,
+            'target_user_id' => $userId,
+        ]);
 
+        return $user
+            && (int) $user->id === (int) $userId;
     }
 );
