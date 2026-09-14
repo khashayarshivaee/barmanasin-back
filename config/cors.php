7c7a1752f5e7cbcc1cd4abf88040722a762d093e
khashayarshivaee <154893891+khashayarshivaee@users.noreply.github.com>
2026-09-14 13:32:34 +0330

    Restore Sanctum token authentication for mail 2

---
+++ config/cors.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    'paths' => [
+
+        'api/*',
+
+        'sanctum/csrf-cookie',
+
+        'broadcasting/auth',
+
+    ],
+
+
+    'allowed_methods' => [
+
+        '*',
+
+    ],
+
+
+    'allowed_origins' => [
+
+        'http://localhost:57630',
+
+        'https://mail.barmanasin.com',
+
+    ],
+
+
+    'allowed_origins_patterns' => [],
+
+
+    'allowed_headers' => [
+
+        '*',
+
+    ],
+
+
+    'exposed_headers' => [],
+
+
+    'max_age' => 0,
+
+
+    'supports_credentials' => true,
+
+];
