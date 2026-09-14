27701c1eb20cd2a67ebacb18f714a72f84978720
khashayarshivaee <154893891+khashayarshivaee@users.noreply.github.com>
2026-09-14 12:05:13 +0330

    Add Sanctum personal access tokens table 7

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,18 +6,35 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
+
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
+
+    ->withBroadcasting(
+        __DIR__.'/../routes/channels.php',
+        [
+            'middleware' => [
+                'web',
+            ],
+        ],
+    )
+
     ->withMiddleware(function (Middleware $middleware): void {
+
         $middleware->statefulApi();
+
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
+
         $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
+            fn (Request $request) =>
+                $request->is('api/*')
+                || $request->expectsJson(),
         );
+
     })->create();
